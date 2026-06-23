84b35c7e4899ecd64495192b3426f84b00f262a0
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-06-23 10:46:47 +0300

    Add custome validation attributes

---
+++ AirportTicketBookingSystem.Models/Attributes/FutureDateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirportTicketBookingSystem.Models.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class FutureDateAttribute : ValidationAttribute
+{
+    public FutureDateAttribute() : base("The {0} must be a future date.") {}
+    public override bool IsValid(object? value)
+    {
+        if (value is DateTime date)
+            return date.Date >= DateTime.Today;
+        return false;
+    }
+
+    public string GetConstraintDescription() => "Required, Allowed Range (today → future)";
+}
