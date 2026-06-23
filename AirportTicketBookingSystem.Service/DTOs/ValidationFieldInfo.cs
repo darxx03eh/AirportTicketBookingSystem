d0f79efc50f847304669ddf331ba5e07cb28835f
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-06-23 11:35:55 +0300

    Add data transfer objects (DTOs)

---
+++ AirportTicketBookingSystem.Service/DTOs/ValidationFieldInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace AirportTicketBookingSystem.Service.DTOs;
+
+public class ValidationFieldInfo
+{
+    public string FieldName { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Constraints { get; set; } = string.Empty;
+}
