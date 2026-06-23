41ef34402bb444e44b954624b626bc0c3f9215b1
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-06-23 10:32:34 +0300

    Project setup with clean build and .gitignore

---
+++ AirportTicketBookingSystem.Infrastructure/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AirportTicketBookingSystem.Infrastructure;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ AirportTicketBookingSystem.Service/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AirportTicketBookingSystem.Service;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
