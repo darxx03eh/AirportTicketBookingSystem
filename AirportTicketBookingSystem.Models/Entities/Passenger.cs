114c2f38e1ff445a404d0c2b7b2068a6f3e658c9
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-06-23 11:06:15 +0300

    Add entity models

---
+++ AirportTicketBookingSystem.Models/Entities/Passenger.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirportTicketBookingSystem.Models.Entities;
+
+public class Passenger
+{
+    public string Id { get; set; } =  Guid.NewGuid().ToString();
+    [Required(ErrorMessage = "First name is required.")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Last name is required.")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; } = string.Empty;
+
+    public string FullName => $"{FirstName} {LastName}";
+}
